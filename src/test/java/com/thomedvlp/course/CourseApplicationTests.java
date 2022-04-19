41b22703d244560fe43cb0b4321209d2bffbcb0a
DevThome <thomedvlp@gmail.com>
2022-04-19 11:09:46 -0300

    first commit

---
+++ src/test/java/com/thomedvlp/course/CourseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.thomedvlp.course;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CourseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
